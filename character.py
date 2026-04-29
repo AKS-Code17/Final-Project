0553d2faddf0a3285137be6c43f05e84629793dc
Sadekar <aarav.sadekar29@bcp.org>
2026-04-29 11:17:26 -0700

    Test 2

---
+++ character.py
@@ -0,0 +1,15 @@
+#Class for sprite in game
+import pygame
+class Character():
+    def __init__(self, x, y, radius, dx, dy, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.dx = dx
+        self.dy = dy
+        self.color = color
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    
